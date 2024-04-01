9c19be2d233001cd3f5c455fc810c99ba6b48491
DeveroCode <charli.rey.misterio@gmail.com>
2024-03-31 18:55:28 -0700

    Update: "Table information with  our projects finished"

---
+++ Front-end/src/components/ProjectsTable.vue
@@ -0,0 +1,118 @@
+<script setup>
+import Logo from '/empresas/didi.png'
+</script>
+
+<template>
+    <div
+        class="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+        <h4 class="mb-6 text-xl font-semibold text-black capitalize">
+            proyectos en desarrollo</h4>
+
+        <table class="flex flex-col">
+            <tr class="grid grid-cols-3 rounded-sm bg-main sm:grid-cols-4">
+                <th class="p-2 xl:p-5">
+                    <h5 class="text-sm font-medium uppercase xs:text-base">Empresa</h5>
+                </th>
+                <th class="p-2 text-center xl:p-5">
+                    <h5 class="text-sm font-medium uppercase xsm:text-base">Inversion</h5>
+                </th>
+                <th class="p-2 text-center xl:p-5">
+                    <h5 class="text-sm font-medium uppercase xsm:text-base">Equipo</h5>
+                </th>
+                <th class="hidden p-2 text-center sm:block xl:p-5">
+                    <h5 class="text-sm font-medium uppercase xsm:text-base">Contacto</h5>
+                </th>
+            </tr>
+            <tr class="grid grid-cols-3 sm:grid-cols-4">
+                <td class="flex items-center gap-3 p-2 xl:p-5 mx-auto">
+                    <!-- <div class="flex-shrink-0">
+                        <img :src="Logo" alt="Brand" class="w-8" />
+                    </div> -->
+                    <p class="text-black ">DeveroCode</p>
+                </td>
+                <td class="flex items-center justify-center p-2 xl:p-5">
+                    <p class="text-black ">234K</p>
+                </td>
+                <td class="flex items-center justify-center p-2 xl:p-5">
+                    <p class="text-meta-3">12</p>
+                </td>
+                <td class="hidden items-center justify-center p-2 sm:flex xl:p-5">
+                    <p class="text-black ">6361216709</p>
+                </td>
+            </tr>
+
+            <tr class="grid grid-cols-3 sm:grid-cols-4">
+                <td class="flex items-center gap-3 p-2 xl:p-5 mx-auto">
+                    <!-- <div class="flex-shrink-0">
+                        <img :src="Logo" alt="Brand" class="w-8" />
+                    </div> -->
+                    <p class="text-black ">DeveroCode</p>
+                </td>
+                <td class="flex items-center justify-center p-2 xl:p-5">
+                    <p class="text-black ">234K</p>
+                </td>
+                <td class="flex items-center justify-center p-2 xl:p-5">
+                    <p class="text-meta-3">12</p>
+                </td>
+                <td class="hidden items-center justify-center p-2 sm:flex xl:p-5">
+                    <p class="text-black ">6361216709</p>
+                </td>
+            </tr>
+            <tr class="grid grid-cols-3 sm:grid-cols-4">
+                <td class="flex items-center gap-3 p-2 xl:p-5 mx-auto">
+                    <!-- <div class="flex-shrink-0">
+                        <img :src="Logo" alt="Brand" class="w-8" />
+                    </div> -->
+                    <p class="text-black ">DeveroCode</p>
+                </td>
+                <td class="flex items-center justify-center p-2 xl:p-5">
+                    <p class="text-black ">234K</p>
+                </td>
+                <td class="flex items-center justify-center p-2 xl:p-5">
+                    <p class="text-meta-3">12</p>
+                </td>
+                <td class="hidden items-center justify-center p-2 sm:flex xl:p-5">
+                    <p class="text-black ">6361216709</p>
+                </td>
+            </tr>
+
+            <tr class="grid grid-cols-3 sm:grid-cols-4">
+                <td class="flex items-center gap-3 p-2 xl:p-5 mx-auto">
+                    <!-- <div class="flex-shrink-0">
+                        <img :src="Logo" alt="Brand" class="w-8" />
+                    </div> -->
+                    <p class="text-black ">DeveroCode</p>
+                </td>
+                <td class="flex items-center justify-center p-2 xl:p-5">
+                    <p class="text-black ">234K</p>
+                </td>
+                <td class="flex items-center justify-center p-2 xl:p-5">
+                    <p class="text-meta-3">12</p>
+                </td>
+                <td class="hidden items-center justify-center p-2 sm:flex xl:p-5">
+                    <p class="text-black ">6361216709</p>
+                </td>
+            </tr>
+
+            <tr class="grid grid-cols-3 sm:grid-cols-4">
+                <td class="flex items-center gap-3 p-2 xl:p-5 mx-auto">
+                    <!-- <div class="flex-shrink-0">
+                        <img :src="Logo" alt="Brand" class="w-8" />
+                    </div> -->
+                    <p class="text-black ">DeveroCode</p>
+                </td>
+                <td class="flex items-center justify-center p-2 xl:p-5">
+                    <p class="text-black ">234K</p>
+                </td>
+                <td class="flex items-center justify-center p-2 xl:p-5">
+                    <p class="text-meta-3">12</p>
+                </td>
+                <td class="hidden items-center justify-center p-2 sm:flex xl:p-5">
+                    <p class="text-black ">6361216709</p>
+                </td>
+            </tr>
+        </table>
+
+
+    </div>
+</template>
