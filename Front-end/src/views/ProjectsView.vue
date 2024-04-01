d7d68df36e1a28b41e8e6c197809d29da7de7368
DeveroCode <charli.rey.misterio@gmail.com>
2024-03-31 18:59:17 -0700

    Update: "finalized tables with false information"

---
+++ Front-end/src/views/ProjectsView.vue
@@ -0,0 +1,13 @@
+<script setup>
+// Components and Views
+import Tables from '@/components/ProjectsTable.vue'
+</script>
+
+<template>
+    <main class="py-20 px-10 space-y-16">
+        <Tables />
+        <Tables />
+        <Tables />
+        <Tables />
+    </main>
+</template>
