fe15f557f7b344aa081f09c470b1002ac3ffb558
DeveroCode <charli.rey.misterio@gmail.com>
2024-04-01 18:43:49 -0700

    Message: "settings to handle laravel as API"

--- Back-end/app/Http/Controllers/AuthController.php
+++ Back-end/app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -9,8 +10,10 @@ class AuthController extends Controller
     //
 
     public function logout(Request $request)
-    {}
-    public function register(Request $request)
+    {
+        $data = $request->validated();
+    }
+    public function register(RegisterRequest $request)
     {}
     public function login(Request $request)
     {}
