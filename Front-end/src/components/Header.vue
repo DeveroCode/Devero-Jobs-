bf7787fb5b4156c1d696ebc80bbe7699707d86d3
DeveroCode <devero@code.com>
2024-04-07 21:38:32 -0600

    Stop design

--- Front-end/src/components/Header.vue
+++ Front-end/src/components/Header.vue
@@ -1,37 +1,153 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+// Imports
+import { ref } from 'vue'
+import { Bars4Icon } from '@heroicons/vue/24/outline'
 import Logo from '/logo.png'
+// Components and Views
+import NavLinks from './NavLinks.vue';
+import NavLinkMobile from './NavLinkMobile.vue';
+
+const open = ref(false);
+
+const toggleMenu = () => {
+    open.value = !open.value;
+    // Si el menú se está cerrando, esperar a que termine la animación y luego ocultarlo
+    if (!open.value) {
+        menuClass.value = 'slideUp';
+        setTimeout(() => {
+            menuClass.value = 'hidden';
+        }, 1000); // Ajusta el tiempo según la duración de la animación
+    } else {
+        menuClass.value = 'slideDown';
+    }
+};
+
 </script>
 
 <template>
-    <header class="flex justify-between items-center py-5 px-20">
-        <img :src="Logo" class="w-32" />
+    <header class="relative flex justify-between items-center px-2 py-5 md:px-20">
+        <img :src="Logo" class="w-32 mt-2 md:mt-0 z-10" />
 
-        <section class="space-x-10 text-gray-500">
-            <RouterLink :to="{ name: 'home' }" class="hover:text-black">Home</RouterLink>
-            <RouterLink :to="{ name: 'home' }" class="hover:text-black">Categorias</RouterLink>
-            <RouterLink :to="{ name: 'home' }" class="hover:text-black">Home</RouterLink>
-            <RouterLink :to="{ name: 'home' }" class="hover:text-black">Home</RouterLink>
-        </section>
+        <!-- NavLinks aparece solo en pantallas medianas -->
+        <NavLinks class="hidden md:flex" />
 
-        <section class="space-x-10">
-            <RouterLink :to="{ name: 'login' }"
-                class="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-buttons rounded-sm shadow-inner group">
-                <span
-                    class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
-                <span
-                    class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
-                <span
-                    class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-                <span
-                    class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-                <span
-                    class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
-                <span class="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Login</span>
-            </RouterLink>
-            <RouterLink :to="{ name: 'register' }" class="rounded-sm border border-header px-2 py-3">
-                Registrarse
-            </RouterLink>
-        </section>
+        <!-- Botón Mobile -->
+        <button @click="toggleMenu" class="z-10 block md:hidden">
+            <Bars4Icon class="w-8" />
+        </button>
+
+        <!-- NavLinkMobile -->
+        <nav class="py-10 w-full absolute top-0 left-0 z-20"
+            :class="open ? 'block bg-white slideDown' : 'slideUp menuClass hidden'">
+            <!-- Aplicar la clase condicionalmente -->
+            <NavLinkMobile :open="open" :toggleMenu="toggleMenu" />
+        </nav>
     </header>
 </template>
+
+<style scoped>
+.slideDown {
+    animation-name: slideDown;
+    -webkit-animation-name: slideDown;
+
+    animation-duration: 1s;
+    -webkit-animation-duration: 1s;
+
+    animation-timing-function: ease;
+    -webkit-animation-timing-function: ease;
+
+    visibility: visible !important;
+}
+
+@keyframes slideDown {
+    0% {
+        transform: translateY(-100%);
+    }
+
+    50% {
+        transform: translateY(1%);
+    }
+
+    65% {
+        transform: translateY(1%);
+    }
+
+    80% {
+        transform: translateY(0%);
+    }
+
+    95% {
+        transform: translateY(0%);
+    }
+
+    100% {
+        transform: translateY(0%);
+    }
+}
+
+@-webkit-keyframes slideDown {
+    0% {
+        -webkit-transform: translateY(-100%);
+    }
+
+    50% {
+        -webkit-transform: translateY(1%);
+    }
+
+    65% {
+        -webkit-transform: translateY(1%);
+    }
+
+    80% {
+        -webkit-transform: translateY(1%);
+    }
+
+    95% {
+        -webkit-transform: translateY(0%);
+    }
+
+    100% {
+        -webkit-transform: translateY(0%);
+    }
+}
+
+
+.slideUp {
+    animation-name: slideUp;
+    -webkit-animation-name: slideUp;
+
+    animation-duration: 1s;
+    -webkit-animation-duration: 1s;
+
+    animation-timing-function: ease;
+    -webkit-animation-timing-function: ease;
+
+    visibility: visible !important;
+}
+
+@keyframes slideUp {
+    0% {
+        transform: translateY(1%);
+    }
+
+    50% {
+        transform: translateY(-90%);
+    }
+
+    65% {
+        transform: translateY(-100%);
+    }
+
+    80% {
+        transform: translateY(-100%);
+    }
+
+    95% {
+        transform: translateY(-100%);
+    }
+
+    100% {
+        transform: translateY(-100%);
+    }
+}
+</style>
---
+++ Front-end/src/components/NavLinks.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+    <section class="hidden md:block space-x-10 text-gray-500">
+        <RouterLink :to="{ name: 'home' }" class="hover:text-black">Home</RouterLink>
+        <RouterLink :to="{ name: 'home' }" class="hover:text-black">Categorias</RouterLink>
+        <RouterLink :to="{ name: 'home' }" class="hover:text-black">Home</RouterLink>
+        <RouterLink :to="{ name: 'home' }" class="hover:text-black">Home</RouterLink>
+    </section>
+
+    <section class="hidden md:block space-x-10">
+        <RouterLink :to="{ name: 'login' }"
+            class="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-buttons rounded-sm shadow-inner group">
+            <span
+                class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+            <span
+                class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+            <span
+                class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+            <span
+                class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+            <span
+                class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
+            <span class="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Login</span>
+        </RouterLink>
+        <RouterLink :to="{ name: 'register' }" class="rounded-sm border border-header px-2 py-3">
+            Registrarse
+        </RouterLink>
+    </section>
+</template>
